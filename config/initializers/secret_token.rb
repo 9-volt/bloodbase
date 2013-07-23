28f757cd09676dd37ba59223afcae1f383c7d8c9
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-24 00:24:22 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloodbase::Application.config.secret_token = '01aef0f03612de9a28385c0e1a8245162a33a16779a3f1150a92421d132d49524f6975a7586e6392b14df69e2459e9911bc8b57800b83c9c394b002dc73b4e47'
---
+++ vagrant/provision.sh
@@ -0,0 +1,39 @@
+if [ -f "/var/vagrant_provision" ]; then
+  # This script will run only if it is the first `vagrant up`
+  # Or if there was a `vagrant destroy` and the VM was destroyed
+  exit 0
+fi
+
+echo "Updating apt-get..."
+apt-get update
+
+echo "Installing the software..."
+apt-get install -y build-essential git curl
+
+echo 'chruby not yet installed, installing now...'
+wget -O chruby-0.3.2.tar.gz https://github.com/postmodern/chruby/archive/v0.3.2.tar.gz
+tar -xzvf chruby-0.3.2.tar.gz
+cd chruby-0.3.2/
+make install
+
+# Add to global profile (enabling chruby for all bash users)
+echo '[ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ] || return' >> /etc/profile.d/chruby.sh
+echo "source /usr/local/share/chruby/chruby.sh" >> /etc/profile.d/chruby.sh
+echo "source /usr/local/share/chruby/auto.sh" >> /etc/profile.d/chruby.sh
+
+cd
+
+wget -O ruby-install-0.2.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.2.1.tar.gz
+tar -xzvf ruby-install-0.2.1.tar.gz
+cd ruby-install-0.2.1/
+make install
+
+su vagrant -c "ruby-install ruby 2.0.0"
+echo 2.0.0 > /home/vagrant/.ruby-version
+
+cd
+source /etc/profile.d/chruby.sh
+
+ruby -v
+
+touch /var/vagrant_provision
