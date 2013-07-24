dda21aec4ed0f814b97d7173cd912a64dce7566a
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-24 12:33:27 +0300

    Move to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bloodbase::Application.config.secret_token = '01aef0f03612de9a28385c0e1a8245162a33a16779a3f1150a92421d132d49524f6975a7586e6392b14df69e2459e9911bc8b57800b83c9c394b002dc73b4e47'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloodbase::Application.config.secret_key_base = 'b1c979cd6af94b539b070f779395915800045041a5746b5a3fd546bf67836110124b36d450b1edd0cbac0778b22ad33113a7ff2462baeff20fe40128bf6f71a9'
