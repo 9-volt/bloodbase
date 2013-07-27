a9d52d118c669b52581a47ed0f6f58f0635593d8
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-27 14:15:31 +0300

    Add centre sections with maps with current location

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Donez::Application.routes.draw do
   root :to => "home#index"
-  get '/faq'  => "home#faq"
 
   devise_for :users
   resources :users, :blood_requests, :donations
 
   get '/help' => "blood_requests#new"
+  get '/ajutor' => 'blood_requests#new'
+
+  get '/:action' => 'home', :as => 'home'
 end
