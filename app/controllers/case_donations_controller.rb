66e5ef6b928408d71fd27827d45f88912aaa7d3e
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-27 23:43:50 +0300

    Link donation form to the campaign

---
+++ app/controllers/case_donations_controller.rb
@@ -0,0 +1,24 @@
+class CaseDonationsController < DonationsController
+  before_filter :fetch_blood_request
+
+  def new
+    @donation = @blood_request.donations.new
+  end
+
+  def create
+    @donation = @blood_request.donations.new donation_params
+    if @donation.save
+      redirect_to donation_path(@donation)
+    else
+      flash.now[:error] = @donation.errors.full_messages.join('<br />')
+                                          .html_safe
+      render 'new'
+    end
+  end
+
+  private
+
+    def fetch_blood_request
+      @blood_request = BloodRequest.find(params[:blood_request_id])
+    end
+end
