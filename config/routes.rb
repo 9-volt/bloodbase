5a74c4c182b73cec0897e9daf9c73bc947ec6090
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-27 13:00:23 +0300

    Add donate basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Donez::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
-  resources :users, :blood_requests
+  resources :users, :blood_requests, :donations
 
   get '/help' => 'blood_requests#new'
 end
