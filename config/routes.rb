ff6201b0a82be5f6982bce5fc6071762ff14b03e
Ivan Danci <idanci@nebulent.com>
2013-07-27 14:42:15 +0300

    Add fuckyou route:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Donez::Application.routes.draw do
   root :to => "home#index"
+  get '/faq'  => "home#faq"
+
   devise_for :users
   resources :users, :blood_requests, :donations
 
-  get '/help' => 'blood_requests#new'
+  get '/help' => "blood_requests#new"
 end
