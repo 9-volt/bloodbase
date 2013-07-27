198aa67521395941f2070daa7258bb85845d9624
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-27 22:37:26 +0300

    Update annotations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -7,6 +7,15 @@
 #  user_id          :integer
 #  created_at       :datetime
 #  updated_at       :datetime
+#  full_name        :string(255)
+#  gender           :string(255)
+#  birthdate        :datetime
+#  blood_group      :string(255)
+#  blood_rh         :string(255)
+#  phone            :string(255)
+#  email            :string(255)
+#  id_number        :string(255)
+#  donate_at        :datetime
 #
 
 class Donation < ActiveRecord::Base
