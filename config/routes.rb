15bbcbf6702986be5452a053d4d643b56bf84ba3
Ivan Danci <bah4uk@gmail.com>
2014-01-12 20:47:42 +0200

    Change ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,22 @@ Donez::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users
+
   resources :users, :donations
   resources :blood_requests do
     resources :case_donations
   end
 
-  get '/help' => "blood_requests#new"
-  get '/ajutor' => 'blood_requests#new'
-  get '/donate' => 'donations#new'
+  get '/help'    => "blood_requests#new"
+  get '/ajutor'  => 'blood_requests#new'
+  get '/donate'  => 'donations#new'
   get '/doneaza' => 'donations#new'
-  get '/cases' => 'blood_requests#index'
+  get '/cases'   => 'blood_requests#index'
 
-  get '/questionnaire' => 'questionnaire#index', :as => 'questionnaire'
+  get '/questionnaire'           => 'questionnaire#index',     :as => 'questionnaire'
   get '/questionnaire/next_item' => 'questionnaire#next_item', :as => 'next_item'
 
-  get '/:action' => 'home', :as => 'home'
+  get '/:action'    => 'home', :as => 'home'
   get '/case/:code' => 'shortlinks#show', :as => 'shortlink'
 
 end
