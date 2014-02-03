4522ff2322f37d33952f99c098762b839a9b7e2c
bumbu <bmbalex@gmail.com>
2014-02-03 21:12:02 +0200

    add collection date to new request

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128200457) do
+ActiveRecord::Schema.define(version: 20140203183613) do
 
   create_table "blood_requests", force: true do |t|
     t.string   "person_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140128200457) do
     t.boolean  "visible",            default: false, null: false
     t.string   "contact_number"
     t.string   "kin"
+    t.date     "collection_date"
   end
 
   create_table "donations", force: true do |t|
