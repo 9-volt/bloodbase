8f9150f3ac661f6b5f8914c49be6cedbfeaa7e7b
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-02-22 20:56:20 +0200

    move encoding before require

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Donez::Application.initialize!
