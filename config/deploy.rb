a783d089f75e37019abd3322fae9c8cf38fc61e5
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-02-22 21:12:07 +0200

    Turns out, it's called linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :log_level,     :info
 set :format,        :pretty
 
 set :linked_files,  %w{ config/database.yml }
-set :linked_folders, %w{ public/system }
+set :linked_dirs,   %w{ public/system }
 set :pty,           true
 set :use_sudo,      false
 set :user,          "donator"
